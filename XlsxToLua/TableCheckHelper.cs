21ec91df69b2e327bd2b97843c6007c5288263f7
mowenmingTXC <mowenming@gmail.com>
2017-07-12 18:58:32 +0800

    增加多表掉落id查询

--- XlsxToLua/TableCheckHelper.cs
+++ XlsxToLua/TableCheckHelper.cs
@@ -2026,7 +2026,7 @@ public class TableCheckHelper
             }
             else
             {
-                string temp = checkRule.CheckRuleString.Substring(START_STRING.Length).Trim();
+                string temp = checkRule.CheckRuleString.Substring(START_STRING.Length).Trim();//去掉前面的fef:字符
                 if (string.IsNullOrEmpty(temp))
                 {
                     errorString = string.Format("值引用检查规则声明错误，\"{0}\"的后面必须跟表格名-字段名\n", START_STRING);
@@ -2042,7 +2042,7 @@ public class TableCheckHelper
                     {
                         // 取出括号中的排除值声明
                         const string EXCEPT_DEFINE_START_STRING = "except";
-                        string exceptDefineString = temp.Substring(leftBracketIndex + 1, rightBracketIndex - leftBracketIndex - 1).Trim();
+                        string exceptDefineString = temp.Substring(leftBracketIndex + 1, rightBracketIndex - leftBracketIndex - 1).Trim();//提取except括号内的声明内容
                         if (!exceptDefineString.StartsWith(EXCEPT_DEFINE_START_STRING, StringComparison.CurrentCultureIgnoreCase))
                         {
                             errorString = string.Format("值引用检查规则声明错误，若要声明ref检查所忽略的特殊值，需在最后以(except{xx,xx})的形式声明，而你在括号中声明为\"{0}\"\n", exceptDefineString);
@@ -2063,18 +2063,199 @@ public class TableCheckHelper
                             temp = temp.Substring(0, leftBracketIndex).Trim();
                         }
                     }
-                    // 解析参考表名、列名声明
+
                     FieldInfo targetFieldInfo = null;
-                    int hyphenIndex = temp.LastIndexOf('-');
-                    if (hyphenIndex == -1)
+
+                    #region 多表多字段情况 ref:table[entry_item.item_id,entry_item_weapon.weapon_id,entry_partner.entry_id](except{0})
+
+                    const string START_STRING2 = "table[";
+                    if (temp.StartsWith(START_STRING2, StringComparison.CurrentCultureIgnoreCase))//如果是以 ref:table开头则
+                    {
+                        temp = temp.Substring(START_STRING2.Length,temp.Length-7).Trim();//提交[]内的表名和字段
+                        if (string.IsNullOrEmpty(temp))
+                        {
+                            errorString = string.Format("值引用检查规则声明错误，\"{0}\"的后面必须跟[表格名.字段名,表格名.字段名]\n", START_STRING2);
+                            return false;
+                        }
+                        //检查表名和字段
+                        //   FieldInfo targetFieldInfo2 = null;
+                        string[] values = temp.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                        for (int i = 0; i < values.Length; ++i)
+                        {
+                            string tempNameField = values[i].Trim();
+                            // 解析参考表名、列名声明
+                            targetFieldInfo = null;
+                            int hyphenIndex = tempNameField.LastIndexOf('.');
+                            if (hyphenIndex == -1)
+                            {
+                                tableName = tempNameField;
+                                fieldIndexDefine = null;
+                            }
+                            else
+                            {
+                                tableName = tempNameField.Substring(0, hyphenIndex).Trim();
+                                fieldIndexDefine = tempNameField.Substring(hyphenIndex + 1, tempNameField.Length - hyphenIndex - 1);
+                            }
+
+                            if (!AppValues.TableInfo.ContainsKey(tableName))
+                            {
+                                errorString = string.Format("值引用检查规则声明错误，找不到名为 {0} 的表格\n", tableName);
+                                return false;
+                            }
+                            if (string.IsNullOrEmpty(fieldIndexDefine))
+                                targetFieldInfo = AppValues.TableInfo[tableName].GetKeyColumnFieldInfo();
+                            else
+                            {
+                                TableInfo targetTableInfo = AppValues.TableInfo[tableName];
+                                targetFieldInfo = GetFieldByIndexDefineString(fieldIndexDefine, targetTableInfo, out errorString);
+                                if (errorString != null)
+                                {
+                                    errorString = string.Format("值引用检查规则声明错误，表格\"{0}\"中无法根据索引字符串\"{1}\"找到要参考的字段，错误信息为：{2}\n", tableName, fieldIndexDefine, errorString);
+                                    return false;
+                                }
+                            }
+                            // 检查目标字段必须为相同的数据类型
+                            if (fieldInfo.DataType != targetFieldInfo.DataType)
+                            {
+                                errorString = string.Format("值引用检查规则声明错误，表格\"{0}\"中通过索引字符串\"{1}\"找到的参考字段的数据类型为{2}，而要检查字段的数据类型为{3}，无法进行不同数据类型字段的引用检查\n", tableName, fieldIndexDefine, targetFieldInfo.DataType.ToString(), fieldInfo.DataType.ToString());
+                                return false;
+                            }
+                        }
+
+                        Dictionary<int, object> unreferencedInfo = new Dictionary<int, object>();
+                        for (int j = 0; j < values.Length; ++j)
+                        {
+                            string tempNameField = values[j].Trim();
+                            // 解析参考表名、列名声明
+                            targetFieldInfo = null;
+                            int hyphenIndex = tempNameField.LastIndexOf('.');
+                            if (hyphenIndex == -1)
+                            {
+                                tableName = tempNameField;
+                                fieldIndexDefine = null;
+                            }
+                            else
+                            {
+                                tableName = tempNameField.Substring(0, hyphenIndex).Trim();
+                                fieldIndexDefine = tempNameField.Substring(hyphenIndex + 1, tempNameField.Length - hyphenIndex - 1);
+                            }
+
+                            if (string.IsNullOrEmpty(fieldIndexDefine))
+                                targetFieldInfo = AppValues.TableInfo[tableName].GetKeyColumnFieldInfo();
+                            else
+                            {
+                                TableInfo targetTableInfo = AppValues.TableInfo[tableName];
+                                targetFieldInfo = GetFieldByIndexDefineString(fieldIndexDefine, targetTableInfo, out errorString);
+                            }
+
+                            List<object> targetFieldData = targetFieldInfo.Data;
+                            // 存储找不到引用对应关系的数据信息（key：行号， value：填写的数据）
+                            Dictionary<int, object> tempunreferencedInfo = new Dictionary<int, object>();
+
+                            if (fieldInfo.DataType == DataType.Int || fieldInfo.DataType == DataType.Long || fieldInfo.DataType == DataType.Float)
+                            {
+                                for (int i = 0; i < fieldInfo.Data.Count; ++i)
+                                {
+                                    // 忽略无效集合元素下属子类型的空值或本身为空值
+                                    if (fieldInfo.Data[i] == null)
+                                        continue;
+                                    // 忽略不进行ref检查的排除值
+                                    else if (exceptValues.Contains(fieldInfo.Data[i]))
+                                        continue;
+
+                                    if (!targetFieldData.Contains(fieldInfo.Data[i]))
+                                        tempunreferencedInfo.Add(i, fieldInfo.Data[i]);
+                                }
+                            }
+                            else if (fieldInfo.DataType == DataType.String)
+                            {
+                                for (int i = 0; i < fieldInfo.Data.Count; ++i)
+                                {
+                                    // 忽略无效集合元素下属子类型的空值以及空字符串
+                                    if (fieldInfo.Data[i] == null || string.IsNullOrEmpty(fieldInfo.Data[i].ToString()))
+                                        continue;
+                                    // 忽略不进行ref检查的排除值
+                                    else if (exceptValues.Contains(fieldInfo.Data[i]))
+                                        continue;
+
+                                    if (!targetFieldData.Contains(fieldInfo.Data[i]))
+                                        tempunreferencedInfo.Add(i, fieldInfo.Data[i]);
+                                }
+                            }
+                            if (tempunreferencedInfo.Count == 0)
+                            {
+                                break;
+                            }
+                            //  var dz = unreferencedInfo.Keys.Intersect(tempunreferencedInfo.Keys);
+                            if (unreferencedInfo.Count > 0)
+                            {
+                                List<int> tempList = new List<int>();
+                                foreach (KeyValuePair<int, object> kvp in unreferencedInfo)
+                                {
+                                    if (tempunreferencedInfo.ContainsKey(kvp.Key))
+                                    {
+                                        //存在处理
+                                    }
+                                    else
+                                    {
+                                        tempList.Add(kvp.Key);
+                                        // unreferencedInfo.Remove(kvp.Key);//不存在就移除
+                                    }
+                                }
+                                foreach (int k in tempList)
+                                {
+                                    unreferencedInfo.Remove(k);//不存在就移除
+                                }
+                            }
+                            else
+                            {
+                                if (tempunreferencedInfo.Count > 0)
+                                {
+                                    foreach (KeyValuePair<int, object> kvp in tempunreferencedInfo)
+                                    {
+                                        unreferencedInfo.Add(kvp.Key, kvp.Value);
+                                    }
+                                }
+                                else
+                                {
+
+                                }
+                            }
+                        }
+
+
+
+
+
+                        if (unreferencedInfo.Count > 0)
+                        {
+                            StringBuilder errorStringBuild = new StringBuilder();
+                            errorStringBuild.AppendLine("存在以下未找到引用关系的数据行：");
+                            foreach (var item in unreferencedInfo)
+                                errorStringBuild.AppendFormat("第{0}行数据\"{1}\"在对应参考列不存在\n", item.Key + AppValues.DATA_FIELD_DATA_START_INDEX + 1, item.Value);
+
+                            errorString = errorStringBuild.ToString();
+                            return false;
+                        }
+                        else
+                        {
+                            errorString = null;
+                            return true;
+                        }
+                    }
+                    #endregion
+                    // 解析参考表名、列名声明
+
+                    int hyphenIndex2 = temp.LastIndexOf('-');
+                    if (hyphenIndex2 == -1)
                     {
                         tableName = temp;
                         fieldIndexDefine = null;
                     }
                     else
                     {
-                        tableName = temp.Substring(0, hyphenIndex).Trim();
-                        fieldIndexDefine = temp.Substring(hyphenIndex + 1, temp.Length - hyphenIndex - 1);
+                        tableName = temp.Substring(0, hyphenIndex2).Trim();
+                        fieldIndexDefine = temp.Substring(hyphenIndex2 + 1, temp.Length - hyphenIndex2 - 1);
                     }
 
                     if (!AppValues.TableInfo.ContainsKey(tableName))
@@ -2108,32 +2289,32 @@ public class TableCheckHelper
 
                         if (fieldInfo.DataType == DataType.Int || fieldInfo.DataType == DataType.Long || fieldInfo.DataType == DataType.Float)
                         {
-                            for (int i = 0; i < fieldInfo.Data.Count; ++i)
+                            for (int i2 = 0; i2 < fieldInfo.Data.Count; ++i2)
                             {
                                 // 忽略无效集合元素下属子类型的空值或本身为空值
-                                if (fieldInfo.Data[i] == null)
+                                if (fieldInfo.Data[i2] == null)
                                     continue;
                                 // 忽略不进行ref检查的排除值
-                                else if (exceptValues.Contains(fieldInfo.Data[i]))
+                                else if (exceptValues.Contains(fieldInfo.Data[i2]))
                                     continue;
 
-                                if (!targetFieldData.Contains(fieldInfo.Data[i]))
-                                    unreferencedInfo.Add(i, fieldInfo.Data[i]);
+                                if (!targetFieldData.Contains(fieldInfo.Data[i2]))
+                                    unreferencedInfo.Add(i2, fieldInfo.Data[i2]);
                             }
                         }
                         else if (fieldInfo.DataType == DataType.String)
                         {
-                            for (int i = 0; i < fieldInfo.Data.Count; ++i)
+                            for (int i3 = 0; i3 < fieldInfo.Data.Count; ++i3)
                             {
                                 // 忽略无效集合元素下属子类型的空值以及空字符串
-                                if (fieldInfo.Data[i] == null || string.IsNullOrEmpty(fieldInfo.Data[i].ToString()))
+                                if (fieldInfo.Data[i3] == null || string.IsNullOrEmpty(fieldInfo.Data[i3].ToString()))
                                     continue;
                                 // 忽略不进行ref检查的排除值
-                                else if (exceptValues.Contains(fieldInfo.Data[i]))
+                                else if (exceptValues.Contains(fieldInfo.Data[i3]))
                                     continue;
 
-                                if (!targetFieldData.Contains(fieldInfo.Data[i]))
-                                    unreferencedInfo.Add(i, fieldInfo.Data[i]);
+                                if (!targetFieldData.Contains(fieldInfo.Data[i3]))
+                                    unreferencedInfo.Add(i3, fieldInfo.Data[i3]);
                             }
                         }
 
--- XlsxToLua/bin/Debug/ExportLua/Hero.lua
+++ XlsxToLua/bin/Debug/ExportLua/Hero.lua
@@ -7,8 +7,8 @@
 -- attributes                       json                             战斗属性
 
 return {
-	[1] = {
-		heroId = 1,
+	[5001] = {
+		heroId = 5001,
 		name = "英雄法师",
 		rare = 11,
 		type = 1,
@@ -46,8 +46,8 @@ return {
 			petPhrase = "I will kill you!",
 		},
 	},
-	[2] = {
-		heroId = 2,
+	[5002] = {
+		heroId = 5002,
 		name = "英雄战士",
 		rare = 11,
 		type = 2,
@@ -55,8 +55,8 @@ return {
 		isOpen = true,
 		attributes = nil,
 	},
-	[3] = {
-		heroId = 3,
+	[5003] = {
+		heroId = 5003,
 		name = "英雄牧师",
 		rare = 11,
 		type = 3,
@@ -64,8 +64,56 @@ return {
 		isOpen = false,
 		attributes = nil,
 	},
-	[4] = {
-		heroId = 4,
+	[5004] = {
+		heroId = 5004,
+		name = "英雄勇士",
+		rare = 11,
+		type = 4,
+		defaultStar = 1,
+		isOpen = false,
+		attributes = {
+			attack = {
+				physical = 140,
+				magic = 0,
+				canCrit = true,
+				hitRate = 0.9,
+				ult = {
+					[1] = {
+						name = "CoupDeGrace",
+						params = {
+							[1] = "sector",
+							[2] = 150,
+							[3] = 0,
+							[4] = true,
+						},
+						cd = 5,
+					},
+				},
+			},
+			defence = {
+				physical = 40,
+				magic = 0,
+			},
+			hp = 150,
+			modelSize = {
+				[1] = 3.5,
+				[2] = 4,
+				[3] = 8,
+			},
+			petPhrase = "Death to all who oppose me!",
+		},
+	},
+	[5005] = {
+		heroId = 5005,
+		name = "英雄牧师",
+		rare = 11,
+		type = 3,
+		defaultStar = 1,
+		isOpen = false,
+		attributes = nil,
+	},
+	[5006] = {
+		heroId = 5006,
 		name = "英雄勇士",
 		rare = 11,
 		type = 4,
